59a0e0a40426c9fdd2cc912c5fb24828ab19c3ed
Robin <imer@imer.cc>
2013-07-26 18:42:58 +0200

    Added copyright notice in all files

--- IQuest.cpp
+++ IQuest.cpp
@@ -1,3 +1,8 @@
+/* This file belongs to the LibM2 library (http://github.com/imermcmaps/LibM2)
+ * Copyright (c) 2013, iMer (www.imer.cc)
+ * All rights reserved.
+ * Licensed under the BSD 3-clause license (http://opensource.org/licenses/BSD-3-Clause)
+ */
 #include "IQuest.hpp"
 tQuestTable IQuest::getTable(){return qTable;}
 void IQuest::add(std::string name,lua_CFunction func){
